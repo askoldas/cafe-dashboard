7e6b30411b1bbb103c77ffd163ad979dd6f74575
Askold <makrikovas@gmail.com>
2025-07-09 22:42:21 +0300

    Move Firebase config to env variables

--- src/firebase.js
+++ src/firebase.js
@@ -3,13 +3,13 @@ import { getAnalytics } from 'firebase/analytics'
 import { getFirestore } from 'firebase/firestore' // ✅ Add this
 
 const firebaseConfig = {
-  apiKey: 'AIzaSyDXkGMXK9DuKEutVS8piegeSy4VXwnYB-M',
-  authDomain: 'cafe-dashboard-337fa.firebaseapp.com',
-  projectId: 'cafe-dashboard-337fa',
-  storageBucket: 'cafe-dashboard-337fa.appspot.com',
-  messagingSenderId: '285955705042',
-  appId: '1:285955705042:web:2470a77ac7751f72e4850b',
-  measurementId: 'G-FE2VRQZRRZ'
+  apiKey: import.meta.env.VITE_FIREBASE_API_KEY,
+  authDomain: import.meta.env.VITE_FIREBASE_AUTH_DOMAIN,
+  projectId: import.meta.env.VITE_FIREBASE_PROJECT_ID,
+  storageBucket: import.meta.env.VITE_FIREBASE_STORAGE_BUCKET,
+  messagingSenderId: import.meta.env.VITE_FIREBASE_MESSAGING_SENDER_ID,
+  appId: import.meta.env.VITE_FIREBASE_APP_ID,
+  measurementId: import.meta.env.VITE_FIREBASE_MEASUREMENT_ID
 }
 
 const app = initializeApp(firebaseConfig)
